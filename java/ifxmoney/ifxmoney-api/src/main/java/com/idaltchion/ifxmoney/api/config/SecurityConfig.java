7b785409735c82ddc8412b9c4b2b7c0827975e8a
idaltchion <idaltchion@gmail.com>
2018-10-16 07:38:27 -0300

    Authentication OAuth2 added
    Java security

--- java/ifxmoney/ifxmoney-api/src/main/java/com/idaltchion/ifxmoney/api/config/SecurityConfig.java
+++ java/ifxmoney/ifxmoney-api/src/main/java/com/idaltchion/ifxmoney/api/config/SecurityConfig.java
@@ -1,28 +1,33 @@
 package com.idaltchion.ifxmoney.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
+/*
+* A anotação ResourceServerConfigurerAdapter gera um WebSecurityConfigurerAdapter.
+* A Order do WebSecurityConfigurerAdapter gerado pelo ResourceServerConfigurerAdapter é 3.
+* A Order deste WebSecurityConfigurerAdapter deve ser > 3, para que o configure(HttpSecurity http) do
+* ResourceServerConfig tenha precedência sobre o configure(HttpSecurity http) do SecurityConfig.
+*/
+@Configuration
 @EnableWebSecurity
+@Order(4)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ROLE");
+		// https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 	}
-	
+
+	@Bean
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/categorias").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().csrf().disable();
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
-	
 }
