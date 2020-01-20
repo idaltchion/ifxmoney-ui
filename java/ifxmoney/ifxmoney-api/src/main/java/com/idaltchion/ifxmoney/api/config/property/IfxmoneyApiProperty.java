bc85f0957f923ddc82047968fd8c13167fed77af
Idaltchion <idaltchion@gmail.com>
2020-01-20 08:10:08 -0300

    UPDATE: Added Profile class and using it as a property. added new prod .properties file

---
+++ java/ifxmoney/ifxmoney-api/src/main/java/com/idaltchion/ifxmoney/api/config/property/IfxmoneyApiProperty.java
@@ -0,0 +1,42 @@
+package com.idaltchion.ifxmoney.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/*
+ * Profile: formas de setar propriedades conforme a necessidade, tal como por tipo de ambiente (dev, prod, etc) entre outras necessidades
+ * Ver utilizacao na classe RefrehTokenPostProcessor
+ */
+@ConfigurationProperties("ifxmoney")
+public class IfxmoneyApiProperty {
+
+	private String origemPermitida = "http://localhost:8000";
+
+	public String getOrigemPermitida() {
+		return origemPermitida;
+	}
+
+	public void setOrigemPermitida(String origemPermitida) {
+		this.origemPermitida = origemPermitida;
+	}
+
+	private final Seguranca seguranca = new Seguranca();
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
+	public static class Seguranca {
+
+		private boolean enableHttps;
+
+		public boolean isEnableHttps() {
+			return enableHttps;
+		}
+
+		public void setEnableHttps(boolean enableHttps) {
+			this.enableHttps = enableHttps;
+		}
+
+	}
+
+}
