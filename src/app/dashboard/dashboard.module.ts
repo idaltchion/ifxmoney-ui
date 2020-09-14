91e476b729589dbb4312a8e4bb0bf07b7a2a47f7
idaltchion <idaltchion@gmail.com>
2020-09-14 08:46:48 -0300

    Created line and pie chart using Chart.js library on the project - wip

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SharedModule } from './../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { SharedModule } from './../shared/shared.module';
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
 
+    PanelModule,
+    ChartModule,
     SharedModule,
     DashboardRoutingModule
   ]
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -6,8 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  pieData = {
+    labels: ['Mensal', 'Educação', 'Lazer'],
+    datasets: [
+      {
+        data: [2500, 2700, 500],
+        backgroundColor: ['#FF9900', '##990099', '#3B3EAC']
+      }
+    ]
+  };
 
-  constructor() { }
+  lineData = {
+      labels: ['Janeiro', 'Fevereiro', 'Agosto'],
+      datasets: [
+        {
+          label: ['linha1'],
+          data: [35, 28, 19],
+          borderColor: '#3366CC'
+        },
+        {
+          label: ['linha2'],
+          data: [12, 56, 59],
+          borderColor: '#D62B00'
+        }
+      ]
+    };
+
+  constructor() {}
 
   ngOnInit() {
   }
